de192cdeba83a8295a89462e0fc42086618214d5
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-07-21 17:17:55 -0300

    bpm purchase request steel project clean up

--- bpm/purchase/request/steel/src/main/java/br/com/altamira/data/serialize/JSonViews.java
+++
@@ -1,7 +0,0 @@
-package br.com.altamira.data.serialize;
-
-public class JSonViews {
-	public static class DefaultView extends JSonViews { }
-	public static class ListView extends DefaultView { }
-	public static class EntityView extends ListView { }
-}
