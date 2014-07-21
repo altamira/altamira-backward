8cb04b6233ba3ddd59f1710cb7de4c7ce13d270a
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-07-21 14:56:51 -0300

    move persistence classes from bpm/purchase/request/steel project to altamira/data project

--- data/service/src/main/java/br/com/altamira/data/serialize/JSonViews.java
+++
@@ -1,6 +0,0 @@
-package br.com.altamira.data.serialize;
-
-public class JSonViews {
-	public static class JsonListView extends JSonViews { }
-	public static class JsonEntityView extends JsonListView { }
-}
